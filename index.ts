0b5c2141ca91b01f80a14c909e59ea8afc366805
Punith Gowda <hi@Punith.me>
2020-05-18 21:34:17 +0530

    deno http server

--- index.ts
+++ index.ts
@@ -2,5 +2,8 @@ import { serve } from "https://deno.land/std@0.50.0/http/server.ts";
 const s = serve({ port: 8000 });
 console.log("http://localhost:8000/");
 for await (const req of s) {
-    req.respond({ body: "Hello World\n" });
+    const res = await fetch('https://api.github.com/users')
+    let data = await res.json()
+    console.log('test- call')
+    req.respond({ body:JSON.stringify(data)});
 }
